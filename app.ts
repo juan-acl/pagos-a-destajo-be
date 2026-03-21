0df44e1f796a3d98a51da950723efa31bc908adb
rknssjdev <166780219+rknssjdev@users.noreply.github.com>
2026-03-20 20:17:30 -0600

    be- Correcciones
    Correcciones en production review con entidad de fechas

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express, { Application } from "express";
+import cors from "cors";
 import { errorMiddleware } from "./src/middlewares/error.middleware";
 import positionWorkerRoutes from "./src/modules/positionWorker/positionWorker.routes";
 import areaRoutes from "./src/modules/area/area.routes";
@@ -13,6 +14,14 @@ const API_PREFIX = "/api";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(`${API_PREFIX}/position-workers`, positionWorkerRoutes);
 app.use(`${API_PREFIX}/area`, areaRoutes);
